97b4d64c60cbccd46edcfc6edbf866d7c3c93ff7
imvlandau <itimvlandau@gmail.com>
2021-07-07 00:49:27 +0200

    amend brand styling and add translation

--- src/components/PmbBrand.js
+++ src/components/PmbBrand.js
@@ -1,44 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/core/styles";
-import Typography from "@material-ui/core/Typography";
-
-const useStyles = makeStyles(theme => ({
-  link: {
-    fontStyle: "italic",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    position: "relative",
-    lineHeight: "1.4em",
-    [theme.breakpoints.down("sm")]: {
-      fontSize: theme.typography.pxToRem(13)
-    },
-    [theme.breakpoints.up("sm")]: {
-      fontSize: theme.typography.pxToRem(14)
-    },
-    textDecoration: "none",
-    fontWeight: "lighter",
-    color: "inherit",
-    overflow: "hidden",
-    whiteSpace: "nowrap",
-    textOverflow: "ellipsis",
-    marginRight: theme.spacing(1),
-    paddingRight: theme.spacing(1 / 4)
-  },
-  symbol: {
-    marginRight: theme.spacing(1 / 4)
-  }
-}));
+import { useTranslation } from "react-i18next";
+import Box from "@material-ui/core/Box";
+import Link from '@material-ui/core/Link';
 
 function PmbBrand({ rotating }) {
-  const classes = useStyles();
+  const { t } = useTranslation();
   return (
-    <Typography color="inherit" variant="h6">
-      <a className={classes.link} href={"/"}>
-        IMV-Landau e. V. - Islamischer multikultureller Verein Landau (الجمعية الاسلامية للثقافات المتعددة مسجد ابي بكر الصديق)
-      </a>
-    </Typography>
+    <Box fontStyle="italic" fontWeight="fontWeightLight" style={{ whiteSpace: 'nowrap' }} mr={1} overflow="hidden" textOverflow="ellipsis">
+      <Link href={"/"} variant="caption" color="inherit" underline="none">
+        IMV-Landau e. V. - {t("brand.description")}
+      </Link>
+    </Box>
   );
 }
 
