978b41b0f0f6475c56dccf334937cf2d1977f25a
imvlandau <itimvlandau@gmail.com>
2021-07-21 15:22:21 +0200

    remove obsolete enhancers

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,24 +1,12 @@
 import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
 import thunkMiddleware from "redux-thunk";
-// import monitorReducersEnhancer from "./enhancers/monitorReducers";
 import rootReducer from "./rootReducer";
 
 export default function configureStore(preloadedState) {
   let middlewares = [thunkMiddleware];
-  // if (process.env.NODE_ENV === `development`) {
-  //   const { createLogger } = require(`redux-logger`);
-  //   const logger = createLogger({
-  //     duration: true,
-  //     diff: true
-  //   });
-  //   middlewares.push(logger);
-  // }
   const middlewareEnhancer = applyMiddleware(...middlewares);
   const enhancers = [middlewareEnhancer];
-  // if (process.env.NODE_ENV === `development`) {
-  //   enhancers.push(monitorReducersEnhancer);
-  // }
   const composedEnhancers = composeWithDevTools(...enhancers);
 
   const store = createStore(
@@ -28,10 +16,6 @@ export default function configureStore(preloadedState) {
     preloadedState,
     composedEnhancers
   );
-
-  // if (process.env.NODE_ENV !== "production" && module.hot) {
-  //   module.hot.accept("./rootReducer", () => store.replaceReducer(rootReducer));
-  // }
 
   return store;
 }
