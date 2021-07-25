bd38f704dedcca9b53dd1707ab8bc49c72f36f25
imvlandau <itimvlandau@gmail.com>
2021-07-25 02:00:09 +0200

    extract notification component into its own directory

---
+++ src/containers/PmbSnackbar/index.js
@@ -0,0 +1,5 @@
+import PmbSnackbar from "./PmbSnackbar";
+import reducer, { initialState } from "./reducer";
+
+export * from "./actions";
+export { PmbSnackbar as default, reducer, initialState, actions };
