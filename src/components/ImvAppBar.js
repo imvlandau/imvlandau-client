e4f45d597f0f6dfa3aed06c63f4bebcfe1025ccc
imvlandau <itimvlandau@gmail.com>
2021-08-08 16:15:28 +0200

    add language dropdown

--- src/components/ImvAppBar.js
+++ src/components/ImvAppBar.js
@@ -4,7 +4,7 @@ import Box from "@material-ui/core/Box";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import ImvBrand from "./ImvBrand";
-// import PmbLanguageDropdown from "./PmbLanguageDropdown";
+import PmbLanguageDropdown from "./PmbLanguageDropdown";
 
 const useStyles = makeStyles(theme => ({
   toolbar: {
@@ -23,7 +23,6 @@ function ImvAppBar() {
         <Toolbar className={classes.toolbar}>
           <ImvBrand />
           <div className={classes.divider} />
-          {/*
           <PmbLanguageDropdown
             languages={[
               { lacc: "de-DE", cca2: "DE", cca3: "DEU" },
@@ -32,7 +31,6 @@ function ImvAppBar() {
             ]}
             defaultLanguage="de-DE"
           />
-          */}
         </Toolbar>
       </AppBar>
     </Box>
