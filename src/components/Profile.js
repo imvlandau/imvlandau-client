b07425576a67fbf30de38c393c7d74031a6c145c
imvlandau <itimvlandau@gmail.com>
2021-09-24 16:46:06 +0200

    amend

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,7 +2,7 @@ import React, { Fragment } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const Profile = () => {
-  const { loading, user } = useAuth0();
+  const { loading, user, ...props } = useAuth0();
 
   if (loading || !user) {
     return <div>Loading...</div>;
@@ -14,7 +14,7 @@ const Profile = () => {
 
       <h2>{user.name}</h2>
       <p>{user.email}</p>
-      <code>{JSON.stringify(user, null, 2)}</code>
+      <pre>{JSON.stringify(user, null, 2)}</pre>
     </Fragment>
   );
 };
