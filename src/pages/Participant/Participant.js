b94d7107b9b31ea083e4a3a65b5bd11c8dc8868b
imvlandau <itimvlandau@gmail.com>
2021-09-25 01:18:42 +0200

    cleanup

--- src/pages/Participant/Participant.js
+++ src/pages/Participant/Participant.js
@@ -3,9 +3,9 @@ import { Helmet } from "react-helmet-async";
 import { connect } from "react-redux";
 import { makeStyles } from "@material-ui/styles";
 import { useTranslation } from "react-i18next";
-import { orange, grey } from '@material-ui/core/colors';
+import { orange, grey } from "@material-ui/core/colors";
 import Button from "@material-ui/core/Button";
-import CircularProgress from '@material-ui/core/CircularProgress';
+import CircularProgress from "@material-ui/core/CircularProgress";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
@@ -21,37 +21,47 @@ import ImvFooter from "../../components/ImvFooter";
 import i18nextInstance from "../../i18nextInstance";
 import * as actionCreators from "./actions";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   heading: {
-    margin: theme.spacing(3, 0)
+    margin: theme.spacing(3, 0),
   },
   stepper: {
     boxShadow: theme.shadows[10],
-    backgroundColor: grey[50]
+    backgroundColor: grey[50],
   },
   buttonStepper: {
     marginTop: theme.spacing(1),
-    marginRight: theme.spacing(1)
+    marginRight: theme.spacing(1),
   },
   actionsContainer: {
     marginTop: theme.spacing(3),
-    marginBottom: theme.spacing(1)
+    marginBottom: theme.spacing(1),
   },
   headingParticipantSettings: {
-    marginTop: theme.spacing(2)
+    marginTop: theme.spacing(2),
   },
   textField: {
     marginTop: theme.spacing(1),
     marginLeft: theme.spacing(1),
-    marginRight: theme.spacing(1)
+    marginRight: theme.spacing(1),
   },
   buttonProgress: {
     marginLeft: theme.spacing(1),
-    color: orange[500]
-  }
+    color: orange[500],
+  },
 }));
 
-function Participant({ notifications, activeStep = 0, qrCodeImageData = null, fetching, fetchSettings, createParticipant, createParticipantFailure, settings, ...props }) {
+function Participant({
+  notifications,
+  activeStep = 0,
+  qrCodeImageData = null,
+  fetching,
+  fetchSettings,
+  createParticipant,
+  createParticipantFailure,
+  settings,
+  ...props
+}) {
   const classes = useStyles();
   const { t } = useTranslation(["participant"]);
   const didMountRef = React.useRef(false);
@@ -64,46 +74,45 @@ function Participant({ notifications, activeStep = 0, qrCodeImageData = null, fe
     companion1: "",
     companion2: "",
     companion3: "",
-    companion4: ""
+    companion4: "",
   });
 
   // ########## stepper
   const handleNext = () => {
-      if (
-        participant.name &&
-        participant.email &&
-        participant.mobile
-      ) {
-        createParticipant({
-            name: participant.name,
-            email: participant.email,
-            mobile: participant.mobile,
-            companion1: participant.companion1,
-            companion2: participant.companion2,
-            companion3: participant.companion3,
-            companion4: participant.companion4
-          },{
-            key: "participant.registration.complete",
-            message: t(
-              "participant.registration.complete"
-            ),
-            type: "success"
-          });
-      } else {
-        createParticipantFailure([{
+    if (participant.name && participant.email && participant.mobile) {
+      createParticipant(
+        {
+          name: participant.name,
+          email: participant.email,
+          mobile: participant.mobile,
+          companion1: participant.companion1,
+          companion2: participant.companion2,
+          companion3: participant.companion3,
+          companion4: participant.companion4,
+        },
+        {
+          key: "participant.registration.complete",
+          message: t("participant.registration.complete"),
+          type: "success",
+        }
+      );
+    } else {
+      createParticipantFailure([
+        {
           key: "participant.form.incomplete",
           message: t("notification.form.incomplete"),
-          type: "error"
-        }]);
-      }
+          type: "error",
+        },
+      ]);
+    }
   };
 
   // ########## participant data
-  const handleChangeParticipant = name => event => {
+  const handleChangeParticipant = (name) => (event) => {
     setParticipant({ ...participant, [name]: event.target.value });
   };
 
-  const formNotFilledOut = notifications.filter(notification => {
+  const formNotFilledOut = notifications.filter((notification) => {
     return notification.key.indexOf("participant.form.incomplete") > -1;
   });
 
@@ -118,10 +127,32 @@ function Participant({ notifications, activeStep = 0, qrCodeImageData = null, fe
   }, [fetching, activeStep, qrCodeImageData, fetchSettings]);
 
   let eventTime = useMemo(() => {
-    return new Date(settings.eventTime1).toLocaleTimeString(i18nextInstance.language, {hour: '2-digit', minute:'2-digit'}) + (settings.eventTime2 && settings.eventTime1 !== settings.eventTime2 ? "/" + new Date(settings.eventTime2).toLocaleTimeString(i18nextInstance.language, {hour: '2-digit', minute:'2-digit'}) : "");
+    return (
+      new Date(settings.eventTime1).toLocaleTimeString(
+        i18nextInstance.language,
+        { hour: "2-digit", minute: "2-digit" }
+      ) +
+      (settings.eventTime2 && settings.eventTime1 !== settings.eventTime2
+        ? "/" +
+          new Date(settings.eventTime2).toLocaleTimeString(
+            i18nextInstance.language,
+            {
+              hour: "2-digit",
+              minute: "2-digit",
+            }
+          )
+        : "")
+    );
   }, [settings]);
   let eventDate = useMemo(() => {
-    return new Date(settings.eventDate).toLocaleDateString(i18nextInstance.language, { weekday: 'long', month: 'long', day: 'numeric' });
+    return new Date(settings.eventDate).toLocaleDateString(
+      i18nextInstance.language,
+      {
+        weekday: "long",
+        month: "long",
+        day: "numeric",
+      }
+    );
   }, [settings]);
 
   return (
@@ -130,13 +161,18 @@ function Participant({ notifications, activeStep = 0, qrCodeImageData = null, fe
       <Notifications />
       <ImvAppBar />
       <Container maxWidth="lg">
-        <Typography className={classes.heading} component="h1" variant="h5" sx={{mt: 1, mb:1}}>
+        <Typography
+          className={classes.heading}
+          component="h1"
+          variant="h5"
+          sx={{ mt: 1, mb: 1 }}
+        >
           {t("participant.event.subject", {
-            eventTopic: settings.eventTopic || '...',
+            eventTopic: settings.eventTopic || "...",
             eventTime,
             eventDate,
-            eventLocation: settings.eventLocation || '...'
-         })}
+            eventLocation: settings.eventLocation || "...",
+          })}
         </Typography>
         <Stepper
           className={classes.stepper}
@@ -148,14 +184,10 @@ function Participant({ notifications, activeStep = 0, qrCodeImageData = null, fe
             <StepLabel>{t("participant.registration.section.name")}</StepLabel>
             <StepContent>
               <Typography variant="h6">
-                {t(
-                  "participant.registration.section.title"
-                )}
+                {t("participant.registration.section.title")}
               </Typography>
               <Typography variant="caption" paragraph>
-                {t(
-                  "participant.registration.section.subtitle"
-                )}
+                {t("participant.registration.section.subtitle")}
               </Typography>
               <form noValidate autoComplete="off">
                 <Grid container spacing={2}>
@@ -206,7 +238,7 @@ function Participant({ notifications, activeStep = 0, qrCodeImageData = null, fe
                 <Typography
                   variant="h6"
                   className={classes.headingParticipantSettings}
-                   sx={{mt: 1}}
+                  sx={{ mt: 1 }}
                 >
                   {t("participant.companions.section.title")}
                 </Typography>
@@ -265,7 +297,13 @@ function Participant({ notifications, activeStep = 0, qrCodeImageData = null, fe
                   className={classes.buttonStepper}
                   disabled={fetching}
                 >
-                  {t("button.next")}{fetching && <CircularProgress size={24} className={classes.buttonProgress} />}
+                  {t("button.next")}
+                  {fetching && (
+                    <CircularProgress
+                      size={24}
+                      className={classes.buttonProgress}
+                    />
+                  )}
                 </Button>
               </div>
             </StepContent>
@@ -275,10 +313,14 @@ function Participant({ notifications, activeStep = 0, qrCodeImageData = null, fe
             <StepContent>
               <Typography variant="caption" paragraph>
                 {t(`participant.confirmation.section.subtitle.beginning`)}
-                 <Box color="warning.dark" component="span" fontFamily="Monospace">{participant.email}</Box>
-                {t(
-                  `participant.confirmation.section.subtitle.end`
-                )}
+                <Box
+                  color="warning.dark"
+                  component="span"
+                  fontFamily="Monospace"
+                >
+                  {participant.email}
+                </Box>
+                {t(`participant.confirmation.section.subtitle.end`)}
               </Typography>
               <Box display="flex" justifyContent="center">
                 <img src={qrCodeImageData} alt="qrcode"></img>
@@ -297,10 +339,7 @@ const mapStateToProps = (state, ownProps) => ({
   fetching: state.participant.fetching,
   qrCodeImageData: state.participant.qrCodeImageData,
   activeStep: state.participant.activeStep,
-  settings: state.settings.data
+  settings: state.settings.data,
 });
 
-export default connect(
-  mapStateToProps,
-  actionCreators
-)(Participant);
+export default connect(mapStateToProps, actionCreators)(Participant);
